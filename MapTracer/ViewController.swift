1d0ba58d616c8e645d079dcd19827fb882823c08
Seyyed Sajjadpour <hadis@seyyeds-imac.resource.campus.njit.edu>
2016-03-29 19:23:38 -0400

    Initial Commit

---
+++ MapTracer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapTracer
+//
+//  Created by Seyyed Sajjadpour on 1/10/1395 AP.
+//  Copyright © 1395 Seyyed Sajjadpour. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
